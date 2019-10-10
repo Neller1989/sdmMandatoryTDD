db609f592b19f174e5ceb2aa77d71cba64961fd3
Neller1989 <43983250+Neller1989@users.noreply.github.com>
2019-10-10 11:14:36 +0200

    test performance

---
+++ MovieRating/UnitTestMovieRating/UnitTestPerformance.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieRating;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestMovieRating
+{
+    [TestClass]
+    public class UnitTestPerformance
+    {
+
+        readonly Logic m = new Logic(@"C:\Users\nikla\Documents\GitHub\sdmMandatoryTDD\MovieRating\MovieRating\Data\ratingsTest1.json");
+
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
